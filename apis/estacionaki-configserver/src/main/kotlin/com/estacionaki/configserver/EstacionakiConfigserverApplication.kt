8b4c630cc5ce49f971ac431192af420b005cab3b
Murilo Carvalho <murilo.carvalho@concrete.com.br>
2019-02-13 16:19:04 -0200

    Add ConfigServer

--- apis/estacionaki-configserver/src/main/kotlin/com/estacionaki/configserver/EstacionakiConfigserverApplication.kt
+++ apis/estacionaki-configserver/src/main/kotlin/com/estacionaki/configserver/EstacionakiConfigserverApplication.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ConfigserverApplication
+class EstacionakiConfigserverApplication
 
 fun main(args: Array<String>) {
-	runApplication<ConfigserverApplication>(*args)
+	runApplication<EstacionakiConfigserverApplication>(*args)
 }
 
--- apis/estacionaki-configserver/src/test/kotlin/com/estacionaki/configserver/EstacionakiConfigserverApplicationTests.kt
+++ apis/estacionaki-configserver/src/test/kotlin/com/estacionaki/configserver/EstacionakiConfigserverApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class ConfigserverApplicationTests {
+class EstacionakiConfigserverApplicationTests {
 
 	@Test
 	fun contextLoads() {
